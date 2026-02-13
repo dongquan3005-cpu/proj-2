0f172be18f563951440c6e8511cc9eaf44ba879c
Quan Nguyen <qnguy8@uic.edu>
2026-02-12 23:15:09 -0600

    initial commit

---
+++ search.cpp
@@ -0,0 +1,60 @@
+#include "include/search.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string cleanToken(const string& token) {
+  string s = token;
+  for (char& c : s) {
+    c = static_cast<char>(tolower(static_cast<unsigned>(c)));
+  }
+
+  bool hasLetter = false;
+  for (char c : s) {
+    if (isalpha(static_cast<unsigned char>(c))) {
+      hasLetter = true;
+      break;
+    }
+  }
+  if (!hasLetter) return "";
+
+  size_t start = 0;
+  while (start < s.size() && ispunct(static_cast<unsigned char>(s[start]))) {
+    start++;
+  }
+
+  size_t end = s.size();
+  while (end > start && ispunct(static_cast<unsigned char>(s[end - 1]))) {
+    end--;
+  }
+
+  return s.substr(start, end - start);
+}
+
+set<string> gatherTokens(const string& text) {
+  // TODO student
+  return {};
+}
+
+int buildIndex(const string& filename, map<string, set<string>>& index) {
+  // TODO student
+  return 0;
+}
+
+set<string> findQueryMatches(const map<string, set<string>>& index,
+                             const string& sentence) {
+  // TODO student
+  return {};
+}
+
+void searchEngine(const string& filename) {
+  // TODO student
+}
---
+++ search_tests.cpp
@@ -0,0 +1,34 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "include/search.h"
+
+using namespace std;
+using namespace testing;
+
+TEST(CleanToken, PunctuationBothEnds) {
+  string input = "!!Hello!!";
+  EXPECT_THAT(cleanToken(input), StrEq("hello")) << "input=\"" << input << "\"";
+}
+
+TEST(CleanToken, PunctuationInMiddle) {
+  string input = "co-op";
+  EXPECT_THAT(cleanToken(input), StrEq("co-op")) << "input=\"" << input << "\"";
+}
+TEST(CleanToken, OnlyNumbers) {
+  string input = "12345";
+  EXPECT_THAT(cleanToken(input), StrEq("")) << "input=\"" << input << "\"";
+}
+TEST(CleanToken, SingleLetter) {
+  string input = "X";
+  EXPECT_THAT(cleanToken(input), StrEq("x")) << "input=\"" << input << "\"";
+}
+TEST(CleanToken, ManyPunctuation) {
+  string input = "......a......";
+  EXPECT_THAT(cleanToken(input), StrEq("a")) << "input=\"" << input << "\"";
+}
+TEST(CleanToken, NumbersAreNotPunctuation) {
+  string input = "1.abc.1";
+  EXPECT_THAT(cleanToken(input), StrEq("1.abc.1"))
+      << "input=\"" << input << "\"";
+}
